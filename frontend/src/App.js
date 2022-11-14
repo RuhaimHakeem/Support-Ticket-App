2bb5449533f123794c5c217b56c0dd9db178f8ee
RuhaimHakeem <ruhaim860@gmail.com>
2022-11-14 13:55:49 +0530

    Added ticket add and view functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Header from "./components/Header";
 import NewTicket from "./pages/NewTicket";
+import Tickets from "./pages/Tickets";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PrivateRoute from "./components/PrivateRoute";
@@ -20,6 +21,9 @@ function App() {
             <Route path="/register" element={<Register />} />
             <Route path="/new-ticket" element={<PrivateRoute />}>
               <Route path="/new-ticket" element={<NewTicket />} />
+            </Route>
+            <Route path="/tickets" element={<PrivateRoute />}>
+              <Route path="/tickets" element={<Tickets />} />
             </Route>
           </Routes>
         </div>
